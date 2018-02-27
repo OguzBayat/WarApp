cfbb491e0c5f4ade72bee8ecf70e6e1504b054ef
Oğuz Bayat <oguzbayat@Oguz-MacBook-Pro.local>
2018-02-28 00:21:10 +0300

    first commit

--- WarApp/ViewController.swift
+++ WarApp/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +31,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func dealTapped(_ sender: Any) {
+        let leftRandomNumber = arc4random_uniform(13) + 2
+        let rightRandomNumber = arc4random_uniform(13) + 2
+        
+        if leftRandomNumber < rightRandomNumber {
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+        }
+        else if leftRandomNumber > rightRandomNumber {
+            leftScore += 1
+            leftScoreLabel.text = String(leftScore)
+        }
+        else {
+            rightScoreLabel.text = String(rightScore)
+            leftScoreLabel.text = String(leftScore)
+        }
+        
+        leftImageView.image = UIImage(named: "card\(leftRandomNumber)")
+        rightImageView.image = UIImage(named: "card\(rightRandomNumber)")
+        
+    }
+    
 }
 
